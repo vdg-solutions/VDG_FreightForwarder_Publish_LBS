fa4a9e2090e55f930490730b71cbf71884e4466b
Quang <vu.minh.quang@outlook.com>
2026-08-23 17:11:01 +0700

    build wasm with opfs sqlite engine feature

--- docs/js/implementations/ui/bootstrap/views/login.js
+++ docs/js/implementations/ui/bootstrap/views/login.js
@@ -42,7 +42,7 @@ function loginHtml() {
         <!-- Footer -->
         <div class="text-[10px] text-slate-300 text-center">
           ${t('login.footer')}
-          <div class="mt-1 font-mono text-slate-400">v0.4.7 (40293b2)</div>
+          <div class="mt-1 font-mono text-slate-400">v0.4.7 (d852d4c)</div>
         </div>
       </div>
     </div>`;
